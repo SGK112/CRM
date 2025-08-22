31249c3647c35c84eb53670fd450fddabfd9f80b
SGK112 <143036545+SGK112@users.noreply.github.com>
2025-08-21 20:21:11 -0700

    fix: Resolve deployment build issues
    - Fix TypeScript error in test-login page by properly handling unknown error type
    - Remove prettier config from ESLint to resolve missing dependency issue
    - Ensure production build compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   root: true,
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    '@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
